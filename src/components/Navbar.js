8cb08b8458108cf7fae696d41d2c9f4b686844d8
deathbed <145954271+pbln@users.noreply.github.com>
2024-08-06 09:52:54 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,30 +17,29 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <div className="navbar-container">
-        <Link to="/" className="navbar-title">
-          <img className='imge'  src='https://archives.bulbagarden.net/media/upload/4/4b/Pok%C3%A9dex_logo.png'/>
-        </Link>
-        
-        <nav>
-          {user && (
-            <div>
-              <Link to="/fav" className="navbar-link">
-                <img src={favImg} className="fav-icon" alt="My favs" />
-              </Link>
-
-              <Link to="/profile" className="navbar-link">
-                <img src={profileImg} className="profile-icon" alt="Profile" />
-              </Link>
-              <button className="nav-button" onClick={handleLogout}>Logout</button>
-            </div>
-          )}
-        </nav>
-        
+      <div className="top-links">
         {!user && (
-          <div className="navbar-links">
+          <>
             <Link to="/login" className="navbar-link">Login</Link>
             <Link to="/signup" className="navbar-link">Signup</Link>
+          </>
+        )}
+      </div>
+      <div className="navbar-container">
+        <Link to="/" className="navbar-title">
+          <img className='logo-image' src='https://archives.bulbagarden.net/media/upload/4/4b/Pok%C3%A9dex_logo.png' alt="Pokedex Logo" />
+        </Link>
+        
+        {user && (
+          <div className="user-links">
+            <Link to="/fav" className="navbar-link">
+              <img src={favImg} className="fav-icon" alt="My favs" />
+            </Link>
+
+            <Link to="/profile" className="navbar-link">
+              <img src={profileImg} className="profile-icon" alt="Profile" />
+            </Link>
+            <button className="nav-button" onClick={handleLogout}>Logout</button>
           </div>
         )}
       </div>
