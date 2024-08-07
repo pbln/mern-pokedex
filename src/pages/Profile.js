db70a53da156fac55955a87241af42d1d09cf21e
deathbed <145954271+pbln@users.noreply.github.com>
2024-08-07 22:05:59 +0530

    Update Profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import { useLogout } from '../hooks/useLogout';
 import { useAuthContext } from '../hooks/useAuthContext';
 import './Profile.css'
 
 export default function Profile() {
+  const { logout } = useLogout();
     const { user } = useAuthContext();
+
+    const handleLogout = () => {
+      logout();
+    };
   return (
     <div className="profile-container">
     <div className="profile-card">
       <h2>Welcome {user?.user?.email} !</h2>
-      <button className="edit-button">Add pokemon</button>
+      <button className="edit-button" onClick={handleLogout}>Logout</button>
     </div>
   </div>
   )
