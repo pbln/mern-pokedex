850e8c6cb165702865b41b4f97ab9bac188800a8
deathbed <145954271+pbln@users.noreply.github.com>
2024-08-12 22:53:16 +0530

    Add files via upload

---
+++ src/customPokemon.js
@@ -0,0 +1,22 @@
+import s1 from './assets/images/s1.png';
+import s2 from './assets/images/s2.png';
+import s3 from './assets/images/s3.png'
+const customPokemons = [
+    {
+      name: 'fishie',
+      abilities: ['Ability 1', 'Ability 2'],
+      types: ['water', 'fire'],
+      customImage: s1 ,
+      height: 10,
+     weight: 100,
+    abt: 'This is a shark cum fish.',
+    evolution: [
+      { name: 'fishie', image: s1 },
+      { name: 'sharfy', image: s2 },
+      { name: 'gyroshark', image: s3 },
+    ],
+    }
+  ]
+  
+  export default customPokemons;
+  
